bd216c671e7399e56dedd03e72d5e3fd5790dce9
MrAlbertus <140925917+MrAlbertus@users.noreply.github.com>
2025-11-05 16:01:49 +0100

    Delete clase 2 directory

--- clase 2/exerciciConstant10.java
+++
@@ -1,6 +0,0 @@
-public class exerciciConstant10 {
-    public static void main(String[] args) {
-        //Realitzar un algorisme perquè calculi l’àrea d’un triangle
-        //cat2 * cat2 = hipotenusa
-    }
-}
